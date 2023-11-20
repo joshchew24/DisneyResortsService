851d75d7b8307ce757f7e2749ef27fc3b3a31cd6
i8w2b <krono@students.cs.ubc.ca>
2023-11-19 20:12:36 -0800

    forgot to commit file

--- public/scripts.js
+++ public/scripts.js
@@ -214,7 +214,7 @@ async function insertReservation(event) {
     });
 
     const responseData = await response.json();
-    const messageElement = document.getElementById('insertReservationResultMsg');
+    const messageElement = document.getElementById('insertionQueryResultMsg');
 
     if (responseData.success) {
         messageElement.textContent = "Data inserted successfully!";
@@ -235,7 +235,7 @@ async function selectAttraction(event) {
     });
 
     const responseData = await response.json();
-    const messageElement = document.getElementById('selectAttractionResultMsg');
+    const messageElement = document.getElementById('selectionQueryResultMsg');
 
     if (responseData.success) {
         const result = responseData.result;
@@ -254,12 +254,33 @@ async function findLandsInAllDisneyResorts() {
     });
 
     const responseData = await response.json();
-    const messageElement = document.getElementById('findAllLandsResultMsg');
+    const messageElement = document.getElementById('divisionQueryResultMsg');
 
     if (responseData.success) {
         messageElement.textContent = `The Lands that appear in all Disney Resorts: ${responseData.result}`;
     } else {
         alert("Error in find lands that appear in all Disney Resorts!");
+    }
+}
+
+// At a specific theme park, find the number of rides with minimumHeight <= x
+async function findNumberOfRidesAtThemeParkWithMinimumHeightLessThanOrEqualToHeight(event) {
+    event.preventDefault();
+
+    const themeParkIdValue = document.getElementById('insertThemeParkId').value;
+    const heightValue = document.getElementById('insertHeight').value;
+
+    const response = await fetch("/find-number-of-rides-at-theme-park-with-minimum-height-less-than-or-equal-to-height", {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+    const messageElement = document.getElementById('aggregateWithHavingQueryResultMsg');
+
+    if (responseData.success) {
+        messageElement.textContent = `Query Results: ${responseData.result}`;
+    } else {
+        alert("Error in Aggregate with Having Query");
     }
 }
 
@@ -275,9 +296,10 @@ window.onload = function() {
     document.getElementById("updataNameDemotable").addEventListener("submit", updateNameDemotable);
     document.getElementById("updateReservation").addEventListener("submit", updateReservation);
     document.getElementById("countDemotable").addEventListener("click", countDemotable);
-    document.getElementById("insertReservation").addEventListener("submit", insertReservation);
-    document.getElementById("selectAttraction").addEventListener("submit", selectAttraction);
-    document.getElementById("findAllLands").addEventListener("click", findLandsInAllDisneyResorts);
+    document.getElementById("insertionQuery").addEventListener("submit", insertReservation);
+    document.getElementById("selectionQuery").addEventListener("submit", selectAttraction);
+    document.getElementById("divisionQuery").addEventListener("click", findLandsInAllDisneyResorts);
+    document.getElementById("aggregateWithHavingQuery").addEventListener("submit", findLandsInAllDisneyResorts);
 };
 
 // General function to refresh the displayed table data. 
